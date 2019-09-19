79a5be112ded47feed4c531e504d7ca75e6f5805
Барыев Дмитрий Викторович <Baryev.D.V@sberbank.ru>
2019-09-19 10:42:34 +0300

    Add first complex correlation analysis. Try to deploy Dash

--- utils.py
+++ utils.py
@@ -1,3 +1,54 @@
+from cmath import *
+
+# Центрирование вещественной величины
+def centering(s):
+    return (s - s.mean())
+
+
+# Центрирование комплекснозначной величины
+def complex_centering(s):
+    return (centering(s.real) + 1j*centering(s.imag))
+
+
+# Приведение вещественной величины к безразмерному виду
+def dimensionless(s):
+    if (s[s!=0].shape[0]==0):
+        return s
+    else: 
+        return (s / s[s!=0].max())
+
+    
+# Приведение комплекснозначной величины к безразмерному виду
+def complex_dimensionless(s):
+    return (dimensionless(s.real) + 1j*dimensionless(s.imag))
+
+
+# Препроцессинг
+def preprocessing(s):
+    s = complex_centering(s)
+    s = complex_dimensionless(s)
+    return s
+
+
+# Нахождение комплексного коэффициента парной корреляции
+def rc(y,x):
+    yr, yi = y.real, y.imag
+    xr, xi = x.real, x.imag
+    
+    rc_xy = (
+        ((yr + 1j*yi)*(xr + 1j*xi)).sum()
+        / 
+        sqrt(((yr + 1j*yi)**2).sum() * ((xr + 1j*xi)**2).sum())
+        )
+    return rc_xy;
+
+
+
+
+#
+# OLD
+#
+
 # Расчет комплексного коэффициента пропорциональности (4.2.11)
 def b(x,y):
     b_xy = (
